da55ab301d621ae8f8ee9b2b31fa00d1ee55eed3
Julien Rousseau <julien-rousseau@hotmail.fr>
2019-07-15 16:57:48 +0200

    book can be set to read even if set by isbn

--- src/Form/BookISBNType.php
+++ src/Form/BookISBNType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,6 +14,14 @@ class BookISBNType extends AbstractType
     {
         $builder
             ->add('ISBN')
+            ->add('isRead', ChoiceType::class, [
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'expanded' => true,
+                'multiple' => false,
+            ])
         ;
     }
 
